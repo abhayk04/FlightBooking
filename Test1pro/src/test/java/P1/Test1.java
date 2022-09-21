c8d315043d6e7560fe045a69c6b04a58ee7d0001
HP <HP@Abhay>
2022-09-21 10:21:32 +0530

    Test1pro Commit

---
+++ Test1pro/src/test/java/P1/Test1.java
@@ -0,0 +1,11 @@
+package P1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Trail Commit Code");
+
+	}
+
+}
